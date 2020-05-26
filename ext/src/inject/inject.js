4eceb30ca0e36a7683db707809b38ae4246f8deb
jorge aguilera <jagedn@gmail.com>
2020-05-26 08:59:16 +0200

    aggresive voyeur mode

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -2,13 +2,17 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {		
-            clearInterval(readyStateCheckInterval);
-            var readyStateCheckInterval = setInterval(function() {                
-                var divs = document.body.querySelectorAll('[data-testid=reply],[data-testid=retweet]');
-                [].forEach.call(divs, function(div) {
-                    div.style.visibility = "hidden";
-                  });
-            }, 5000)			
-		}
+                  clearInterval(readyStateCheckInterval);
+                  var readyStateCheckInterval = setInterval(function() {                
+                        var divs = document.body.querySelectorAll('[data-testid=toolBar]');
+                        [].forEach.call(divs, function(div) {
+                              div.style.visibility = "hidden";
+                        });
+                        divs = document.body.querySelectorAll('.DraftEditor-root');
+                        [].forEach.call(divs, function(div) {
+                              div.style.visibility = "hidden";
+                        });
+                  }, 5000)
+            }
 	}, 10);
 });
