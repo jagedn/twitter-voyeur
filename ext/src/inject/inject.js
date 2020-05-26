7a75e99bfc37f1cac77fd2b842b5de5dce6c4bfe
jorge aguilera <jagedn@gmail.com>
2020-05-26 10:22:59 +0200

    v0.0.3

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -3,8 +3,12 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "complete") {		
                   clearInterval(readyStateCheckInterval);
-                  var readyStateCheckInterval = setInterval(function() {                
-                        var divs = document.body.querySelectorAll('[data-testid=toolBar]');
+                  var readyStateCheckInterval = setInterval(function() {    
+                        var divs = document.body.querySelectorAll('[data-testid=reply],[data-testid=retweet]');
+                        [].forEach.call(divs, function(div) {
+                              div.style.visibility = "hidden";
+                        });            
+                        divs = document.body.querySelectorAll('[data-testid=toolBar]');
                         [].forEach.call(divs, function(div) {
                               div.style.visibility = "hidden";
                         });
